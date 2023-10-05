663a3199b6abc499981d439f50ed95141700b539
matthew-collett <97645707+matthew-collett@users.noreply.github.com>
2023-10-05 13:40:23 -0300

    feat: Add more fields in Post

--- app/src/main/java/com/playloudr/app/models/Post.kt
+++ app/src/main/java/com/playloudr/app/models/Post.kt
@@ -3,7 +3,14 @@ package com.playloudr.app.models
 data class Post(
   val username: String,
   val timestamp: String,
+  val title: String,
+  val artist: String,
   val caption: String?,
   val imageUrl: String,
-  val audioUrl: String?
+  val audioUrl: String?,
+  val type: Type
 )
+
+enum class Type {
+  SONG, ALBUM, EP
+}
