f39fdf376179e3078409b4931a9f6edd93a2b2b5
DESKTOP-B63HNU2\Eric <ecuenat@unb.ca>
2023-10-18 22:12:32 -0300

    parking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
   implementation("com.typesafe:config:1.4.1")
   implementation("androidx.core:core-ktx:+")
   testImplementation("junit:junit:4.13.2")
+  implementation("androidx.compose.material:material-icons-extended:1.5.3")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
   androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
