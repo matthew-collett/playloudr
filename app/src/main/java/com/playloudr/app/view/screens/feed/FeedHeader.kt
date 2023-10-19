99d850215ba9f242919b171cee3d24a83bb23f6c
DESKTOP-B63HNU2\Eric <ecuenat@unb.ca>
2023-10-18 23:43:59 -0300

    start live feed, navbar and header

--- app/src/main/java/com/playloudr/app/view/screens/feed/FeedHeader.kt
+++ app/src/main/java/com/playloudr/app/view/screens/feed/FeedHeader.kt
@@ -1,31 +1,17 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.icons.sharp.Search
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.playloudr.app.R
@@ -55,30 +41,16 @@ fun FeedHeader(
       contentScale = ContentScale.Crop
     )
 
-//        Icon(
-//          Icons.Default.Search,
-//          contentDescription = "Search Bar",
-//          modifier = Modifier
-//            .padding(end = 16.dp)
-//            .size(30.dp),
-//            //.clickable()
-//
-//
-//        )
-
     Icon(
-      imageVector = Icons.Sharp.Search, // Here we use the predefined material icon for search
-      contentDescription = "Search" // accessibility description
+      painter = painterResource(id = R.drawable.ic_playloudr_search_icon),
+      contentDescription = "search bar",
+      modifier = Modifier
+        .size(18.dp)
+        .clickable {  }
     )
 
-//    Image(
-//      painter = painterResource(id = R.drawable.ic_playloudr_search_icon), // Replace with your actual search icon resource
-//      contentDescription = "Search",
-//      modifier = Modifier
-//        .size(30.dp), // Adjust the size as needed
-//      contentScale = ContentScale.Crop
-//
-//    )
+
+
     //Button(onClick = { /*TODO*/ }) {}
 
 
--- app/src/main/java/com/playloudr/app/view/screens/feed/Navbar.kt
+++ app/src/main/java/com/playloudr/app/view/screens/feed/Navbar.kt
@@ -1,60 +1,70 @@
-//package com.playloudr.app.view.screens.feed
-//
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.layout.Box
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.material.BottomNavigation
-//import androidx.compose.material.BottomNavigationItem
-//import androidx.compose.material.Icon
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.AccountCircle
-//import androidx.compose.material.icons.filled.AddCircleOutline
-//import androidx.compose.material.icons.filled.Home
-//import androidx.compose.runtime.*
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.tooling.preview.Preview
-//
-//@Composable
-//fun BottomNavBar() {
-//    var selectedTab by remember { mutableStateOf(0) }
-//
-//    BottomNavigation(
-//        backgroundColor = Color.White,
-//        contentColor = Color.Black,
-//        modifier = Modifier.fillMaxWidth()
-//    ) {
-//        BottomNavigationItem(
-//            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home") },
-//            selected = selectedTab == 0,
-//            onClick = { selectedTab = 0 }
-//        )
-//
-//        BottomNavigationItem(
-//            icon = { Icon(imageVector = Icons.Default.AddCircleOutline, contentDescription = "Post") },  // Changed to AddCircleOutline
-//            selected = selectedTab == 1,
-//            onClick = { selectedTab = 1 }
-//        )
-//
-//        BottomNavigationItem(
-//            icon = { Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "View Account") },
-//            selected = selectedTab == 2,
-//            onClick = { selectedTab = 2 }
-//        )
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun PreviewBottomNavBar() {
-//    Box(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .background(Color(0xFFF2F2F2)),
-//        contentAlignment = Alignment.BottomCenter
-//    ) {
-//        BottomNavBar()
-//    }
-//}
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Add
+import androidx.compose.material.icons.sharp.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playloudr.app.R
+
+@Composable
+fun Navbar(
+//    onAddClick: () -> Unit = {},
+//    onChatClick: () -> Unit = {}
+) {
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(56.dp)
+      .padding(16.dp),
+    horizontalArrangement = Arrangement.SpaceBetween,
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+
+
+
+    Icon(
+      painter = painterResource(id = R.drawable.ic_playloudr_home), // Here we use the predefined material icon for search
+      contentDescription = "Search", // accessibility description
+      modifier = Modifier
+        .size(24.dp)
+
+    )
+
+    Icon(
+      painter = painterResource(id = R.drawable.ic_playloudr_plus), // Here we use the predefined material icon for search
+      contentDescription = "Search", // accessibility description
+      modifier = Modifier
+        .size(24.dp)
+    )
+
+    Icon(
+      painter = painterResource(id = R.drawable.ic_playloudr_user), // Here we use the predefined material icon for search
+      contentDescription = "View Account Nav",
+      modifier = Modifier
+        .size(30.dp)
+    )
+
+
+
+
+
+  }
+}
+
+@Preview
+@Composable
+fun NavbarPreview() {
+  Navbar()
+}
