aa4c9e641781dc18ce95590852146578ff7cadab
DESKTOP-B63HNU2\Eric <ecuenat@unb.ca>
2023-10-22 16:03:53 -0300

    parking

--- app/src/main/java/com/playloudr/app/MainActivity.kt
+++ app/src/main/java/com/playloudr/app/MainActivity.kt
@@ -1,25 +1,19 @@
 package com.playloudr.app
 
-import FeedHeader
 import Navbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.playloudr.app.model.entities.posts
-import com.playloudr.app.model.repository.PostRepository
 import com.playloudr.app.view.screens.feed.FeedScreen
 import com.playloudr.app.view.theme.PlayloudrTheme
 
@@ -32,7 +26,7 @@ class MainActivity : ComponentActivity() {
         Surface(modifier = Modifier.fillMaxSize(),
           //color = MaterialTheme.colorScheme.background
         ) {
-          FeedScreen(posts)
+          FeedScreen(posts, modifier = Modifier.fillMaxHeight())
           Navbar()
         }
       }
@@ -66,14 +60,23 @@ class MainActivity : ComponentActivity() {
 @Preview
 fun GreetingPreview() {
   PlayloudrTheme {
-    Column(
-      modifier = Modifier
-        .fillMaxSize()
-        .padding(8.dp)
-    ) {
-      FeedScreen(posts)
-      Spacer(modifier = Modifier.weight(1f))
+    Column(modifier = Modifier.fillMaxSize()) {
+      Box( modifier = Modifier.weight(1f).fillMaxWidth() ) {
+        FeedScreen(postList = posts, modifier = Modifier)
+      }
       Navbar()
     }
+
+//    Column(
+//      modifier = Modifier
+//        .fillMaxSize()
+//        .padding(8.dp)
+//    ) {
+//      FeedScreen(posts,
+//        modifier = Modifier.weight(if)
+//      )
+//
+//      Navbar()
+//    }
   }
 }
