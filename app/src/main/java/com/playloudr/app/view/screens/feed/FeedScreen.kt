f6b77d74833270d9d72426c88dc138d9228e2c0f
Matthew Collett <matthew.collett12@gmail.com>
2023-10-22 16:13:57 -0300

    parking

--- app/src/main/java/com/playloudr/app/view/screens/feed/FeedScreen.kt
+++ app/src/main/java/com/playloudr/app/view/screens/feed/FeedScreen.kt
@@ -1,23 +1,25 @@
 package com.playloudr.app.view.screens.feed
 
-import FeedHeader
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.playloudr.app.R
 import com.playloudr.app.model.entities.PostEntity
-import com.playloudr.app.view.theme.PlayloudrTheme
-import com.playloudr.app.view.theme.myColors
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter", "UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -25,12 +27,28 @@ fun FeedScreen(postList: List<PostEntity>, modifier: Modifier) {
   Scaffold(
     topBar = {
       TopAppBar(
-        title = { FeedHeader() },
+        title = {
+          Image(
+            painter = painterResource(id = R.drawable.ic_playloudr_logo),
+            contentDescription = "App Logo",
+            modifier = Modifier
+              .size(120.dp),
+            contentScale = ContentScale.Crop
+          )
+                },
         backgroundColor = Color.White,
-        //navigationIcon = null
+        actions = {
+          IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+              painter = painterResource(id = R.drawable.ic_playloudr_search_icon),
+              contentDescription = "search bar",
+              modifier = Modifier
+                .size(18.dp)
+            )
+          }
+        }
       )
     }
-    //bottomBar = { BottomBar() }
   ) {
     LazyColumn(
       modifier = Modifier
