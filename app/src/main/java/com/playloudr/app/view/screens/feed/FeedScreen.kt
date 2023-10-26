eab4ff5bc9a246c31e96c7c0fe0939e21bff14d2
DESKTOP-B63HNU2\Eric <ecuenat@unb.ca>
2023-10-26 16:43:03 -0300

    fix: improve top bar collapse

--- app/src/main/java/com/playloudr/app/view/screens/feed/FeedScreen.kt
+++ app/src/main/java/com/playloudr/app/view/screens/feed/FeedScreen.kt
@@ -3,16 +3,27 @@ package com.playloudr.app.view.screens.feed
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.playloudr.app.model.entities.PostEntity
+import com.playloudr.app.util.Debouncer
 import com.playloudr.app.view.components.ScrollAwareLazyColumn
 
 @Composable
-fun FeedScreen(postList: List<PostEntity>, onScrollDown: () -> Unit, onScrollUp: () -> Unit) {
+fun FeedScreen(
+  postList: List<PostEntity>,
+  onScrollDown: () -> Unit,
+  onScrollUp: () -> Unit
+) {
+  val coroutineScope = rememberCoroutineScope()
+  // 15 ms optimal
+  val debouncer = remember { Debouncer(waitMs = 15, coroutineScope) }
+
   ScrollAwareLazyColumn(
     modifier = Modifier.fillMaxWidth(),
-    scrollUp = onScrollUp,
-    scrollDown = onScrollDown
+    scrollUp = { debouncer.debounce(onScrollUp) },
+    scrollDown = { debouncer.debounce(onScrollDown) }
   ) {
     items(postList) { post ->
       PostCard(post)
