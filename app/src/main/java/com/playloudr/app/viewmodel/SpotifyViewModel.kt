38e8cdb10234d3c90366467b2c11f5ceb6547cc3
DESKTOP-B63HNU2\Eric <ecuenat@unb.ca>
2023-11-15 23:09:39 -0400

    parking

--- app/src/main/java/com/playloudr/app/viewmodel/SpotifyViewModel.kt
+++ app/src/main/java/com/playloudr/app/viewmodel/SpotifyViewModel.kt
@@ -1,12 +1,15 @@
 package com.playloudr.app.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.playloudr.app.model.repository.PostRepository
 import com.playloudr.app.model.repository.SpotifyRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SpotifyViewModel() : ViewModel() {
 
@@ -14,20 +17,25 @@ class SpotifyViewModel() : ViewModel() {
   val songImageUrl: StateFlow<String?> = _songImageUrl
   fun searchSong(songName: String) {
     viewModelScope.launch {
-      // First, get the access token
-      val accessToken = SpotifyRepository.getSpotifyAccessToken()
-      if (accessToken != null) {
-        // If the token retrieval was successful, search for the song
-        val imageUrl = SpotifyRepository.searchSong(songName)
-        _songImageUrl.value = imageUrl
-      } else {
-        // Handle the error (e.g., by updating some error state in the ViewModel)
-        _songImageUrl.value = null
+      // need to use Dispatchers.IO since it is network call
+      withContext(Dispatchers.IO) {
+        // First, get the access token
+        val accessToken = SpotifyRepository.getSpotifyAccessToken()
+        if (accessToken != null) {
+          Log.d("SpotifyViewModel", "access token success in SpotifyViewModel")
+          // If the token retrieval was successful, search for the song
+          val imageUrl = SpotifyRepository.searchSong(songName)
+          withContext(Dispatchers.Main) {
+            _songImageUrl.value = imageUrl
+          }
+        } else {
+          Log.e("SpotifyViewModel", "access token received is null in SpotifyViewModel")
+          withContext(Dispatchers.Main) {
+            _songImageUrl.value = null
+          }
+        }
       }
     }
   }
 
-  fun getAccessToken() {
-
-  }
 }
