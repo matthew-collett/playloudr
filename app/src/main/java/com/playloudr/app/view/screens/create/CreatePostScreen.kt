d35ade87b0f515b062310ffc68d6e7f566d297b5
DESKTOP-B63HNU2\Eric <ecuenat@unb.ca>
2023-11-18 19:01:56 -0400

    Contains feat-dataui feat-spotify and feat-signin

--- app/src/main/java/com/playloudr/app/view/screens/create/CreatePostScreen.kt
+++ app/src/main/java/com/playloudr/app/view/screens/create/CreatePostScreen.kt
@@ -1,8 +1,110 @@
 package com.playloudr.app.view.screens.create
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.playloudr.app.R
 import com.playloudr.app.viewmodel.CreatePostViewModel
+import com.playloudr.app.viewmodel.ProfileViewModel
 
 @Composable
-fun CreatePostScreen(viewModel: CreatePostViewModel) {
+fun CreatePostScreen(
+  viewModel: CreatePostViewModel,
+  navController: NavController
+) {
+  var imageUri by remember { mutableStateOf<ImageBitmap?>(null) }
+  var caption by remember { mutableStateOf(TextFieldValue()) }
+  val context = LocalContext.current
+
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(16.dp),
+    verticalArrangement = Arrangement.spacedBy(10.dp)
+  ) {
+    // Placeholder for the image with click event to select an image
+    Box(
+      modifier = Modifier
+        .fillMaxWidth()
+        .height(250.dp)
+        .clickable { /* Launch image picker here */ },
+      contentAlignment = Alignment.Center
+    ) {
+      if (imageUri != null) {
+        // Display selected image
+        Image(
+          bitmap = imageUri!!,
+          contentDescription = "Selected Image",
+          modifier = Modifier.fillMaxSize()
+        )
+      } else {
+        // Display placeholder for image
+        Icon(
+          painter = painterResource(id = R.drawable.ic_playloudr_logo), // Replace with camera icon resource
+          contentDescription = "Placeholder Image",
+          tint = Color.Gray,
+          modifier = Modifier.size(60.dp)
+        )
+      }
+    }
+
+    // Text field for the caption
+    TextField(
+      value = caption,
+      onValueChange = { caption = it },
+      label = { Text("Enter a caption...") },
+      maxLines = 4,
+      keyboardOptions = KeyboardOptions.Default.copy(
+        imeAction = ImeAction.Done
+      ),
+      keyboardActions = KeyboardActions(onDone = {
+        // Handle the done action of the keyboard
+      }),
+      modifier = Modifier.fillMaxWidth()
+    )
+
+    // Button to create a post
+    Button(
+      onClick = {
+        // Handle the post creation here
+      },
+      modifier = Modifier.align(Alignment.End)
+    ) {
+      Text("Post")
+    }
+  }
 }
+
+// Placeholder for the camera icon resource
+//@Composable
+//fun CameraIcon() {
+//  Icon(
+//    imageVector = Icons.Default.CameraAlt,
+//    contentDescription = "Camera Icon"
+//  )
+//}
+
+//@Composable
+//@Preview
+//fun CreatePostScreenPreview() {
+//  CreatePostScreen()
+//}
--- app/src/main/java/com/playloudr/app/view/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/playloudr/app/view/screens/profile/ProfileScreen.kt
@@ -1,21 +1,32 @@
 package com.playloudr.app.view.screens.profile
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,13 +36,19 @@ import com.playloudr.app.model.entities.reecher
 import com.playloudr.app.model.entities.reecherPosts
 import com.playloudr.app.view.screens.feed.FeedTopBar
 import com.playloudr.app.viewmodel.ProfileViewModel
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ProfileScreen(
   viewModel: ProfileViewModel,
   navController: NavController
   ) {
+  var showDrawer by remember { mutableStateOf(false) }
+  val scaffoldState = rememberScaffoldState()
+  val scope = rememberCoroutineScope()
+
   // for top bar
   val displayName = reecher.displayName
   // for header
@@ -40,34 +57,63 @@ fun ProfileScreen(
   val bio = reecher.bio
   // lazy col
   val scrollState = rememberLazyListState()
-  LazyColumn(
-    state = scrollState,
-    modifier = Modifier
-      .fillMaxSize()
-      .background(Color.White),
-    verticalArrangement = Arrangement.spacedBy(16.dp)
+
+  val onLogoutClick = {
+    // Handle logout click holder
+  }
+
+  Scaffold(
+    scaffoldState = scaffoldState,
+    topBar = {
+      ProfileTopBar(username = displayName!!) {
+        showDrawer = !showDrawer
+      }
+    },
+
   ) {
-    // username (header)
-    stickyHeader { ProfileTopBar(displayName!!) }
+    Box(Modifier.padding(vertical = 16.dp)) {
+      LazyColumn(
+        state = scrollState,
+        modifier = Modifier
+          .fillMaxSize()
+          .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+      ) {
+        // profile picture, name, followers, following, posts, bio
+        item {
+          ProfileHeader(profilePic, name, bio)
+        }
 
-    // profile picture, name, followers, following, posts, bio
-    item {
-      ProfileHeader(profilePic, name, bio)
-    }
+        // Grid of posts
+        gridItems(
+          data = reecherPosts,
+          columnCount = 2
+        ) { post ->
+          ProfilePostCard(post) { clickedPost ->
+            navController.navigate("postDetail/${clickedPost.postId}")
+          }
+        }
 
-    // Grid of posts
-    gridItems(
-      data = reecherPosts,
-      columnCount = 2
-    ) { post ->
-      ProfilePostCard(post) { clickedPost ->
-        navController.navigate("postDetail/${clickedPost.postId}")
+      }
+      AnimatedVisibility(
+        visible = showDrawer,
+        enter = slideInHorizontally(initialOffsetX = { fullWidth -> fullWidth }),
+        exit = slideOutHorizontally(targetOffsetX = { fullWidth -> fullWidth }),
+        modifier = Modifier
+          .align(Alignment.CenterEnd)
+          .background(Color.White)
+      ) {
+        // Drawer content
+        DrawerContent(
+          onLogoutClick = {
+            onLogoutClick()
+            showDrawer = false // Close the drawer
+          }
+        )
       }
     }
 
-
   }
-
 }
 
 // helper function to implement profile grid
